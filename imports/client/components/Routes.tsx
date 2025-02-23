ad461d7fa44ca3eedce1d6f7068ae4c9b0157898
James <js@sugrono.com>
2025-02-23 19:19:10 +1100

    Add a list of pending users At the moment, this is just informational. In future, we may allow for invitations to be reissued, or for the invitation link to be extracted from the UI.

--- imports/client/components/Routes.tsx
+++ imports/client/components/Routes.tsx
@@ -1,3 +1,5 @@
+import { Meteor } from "meteor/meteor";
+import { useTracker } from "meteor/react-meteor-data";
 import React, { Suspense } from "react";
 import type { RouteObject } from "react-router-dom";
 import { Navigate, useRoutes } from "react-router-dom";
@@ -14,6 +16,7 @@ import HuntListApp from "./HuntListApp";
 import HuntListPage from "./HuntListPage";
 import HuntProfileListPage from "./HuntProfileListPage";
 import HuntersApp from "./HuntersApp";
+import InvitedUserListPage from "./InvitedUserListPage";
 import JoinHunt from "./JoinHunt";
 import Loading from "./Loading";
 import LoginForm from "./LoginForm";
@@ -24,15 +27,11 @@ import ProfilePage from "./ProfilePage";
 import PuzzleListPage from "./PuzzleListPage";
 import PuzzlePage from "./PuzzlePage";
 import RootRedirector from "./RootRedirector";
+import TagBulkEditPage from "./TagBulkEditPage";
+import TagManagerPage from "./TagManagerPage";
 import UserInvitePage from "./UserInvitePage";
 import UsersApp from "./UsersApp";
 import { AuthenticatedPage, UnauthenticatedPage } from "./authentication";
-import TagManagerApp from "./TagManagerApp";
-import TagManagerPage from "./TagManagerPage";
-import TagEditPage from "./TagEditPage";
-import TagBulkEditPage from "./TagBulkEditPage";
-import { useTracker } from "meteor/react-meteor-data";
-import { Meteor } from "meteor/meteor";
 
 const HuntEditPage = React.lazy(() => import("./HuntEditPage"));
 const SetupPage = React.lazy(() => import("./SetupPage"));
@@ -99,6 +98,7 @@ export const AuthenticatedRouteList: RouteObject[] = [
     children: [
       { path: ":userId", element: <ProfilePage /> },
       { path: "", element: <AllProfileListPage /> },
+      { path: "invited", element: <InvitedUserListPage /> },
     ],
   },
   { path: "/setup", element: <SetupPage /> },
