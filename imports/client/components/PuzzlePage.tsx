0425713ad0133f76754c5bdffaf2987e181055e0
James <js@sugrono.com>
2025-04-23 23:38:56 +1000

    Add chat attachments panel

--- imports/client/components/PuzzlePage.tsx
+++ imports/client/components/PuzzlePage.tsx
@@ -1799,6 +1799,10 @@ const ChatInput = React.memo(
       return undefined;
     }, [parentMessage, displayNames]);
 
+    const showSendButton = useTracker(()=>{
+      return imagePreviews.length > 0 || JSON.stringify(content) !== JSON.stringify(initialValue);
+    }, [imagePreviews.length, content, initialValue])
+
     return (
       <ChatInputRow>
       {replyingTo && parentSenderName && (
@@ -1848,14 +1852,19 @@ const ChatInput = React.memo(
           />
             <FormGroup>
           <ButtonGroup>
-          <Button
-            variant="secondary"
-            onClick={sendContentMessage}
-            onMouseDown={preventDefaultCallback}
-            disabled={disabled || !hasNonTrivialContent}
-          >
-          {isUploading ? <FontAwesomeIcon icon={faSpinner} spin /> : <FontAwesomeIcon icon={faPaperPlane} />}
-          </Button>
+            {
+              showSendButton &&
+                (
+                  <Button
+                    variant="secondary"
+                    onClick={sendContentMessage}
+                    onMouseDown={preventDefaultCallback}
+                    disabled={disabled || !hasNonTrivialContent}
+                  >
+                  {isUploading ? <FontAwesomeIcon icon={faSpinner} spin /> : <FontAwesomeIcon icon={faPaperPlane} />}
+                  </Button>
+                )
+            }
           {s3Configured && (
             <>
               <FormControl
