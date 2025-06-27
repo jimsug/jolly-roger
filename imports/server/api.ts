f734515c759fb07b2f5ff90787be00beb3e0f66a
James <js@sugrono.com>
2025-06-27 11:14:32 +1000

    Extension updates - Added bulk add (useful for that initial add) - Added crx/xpi building

--- imports/server/api.ts
+++ imports/server/api.ts
@@ -1,12 +1,9 @@
 import express from "express";
 import authenticator from "./api/authenticator";
-<<<<<<< HEAD
-import updatePuzzleNote from "./api/resources/updatePuzzleNote";
-=======
 import createPuzzle from "./api/resources/createPuzzle";
 import hunts from "./api/resources/hunts";
 import tags from "./api/resources/tags";
->>>>>>> jpd/extension
+import updatePuzzleNote from "./api/resources/updatePuzzleNote";
 import users from "./api/resources/users";
 
 const api = express();
