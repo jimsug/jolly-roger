1df5e89b88afd81ec5c6e070b18bb41e37c66460
James <js@sugrono.com>
2025-10-11 23:48:23 +1100

    Updates to server-side things; mainly findOneAsync changes

--- imports/server/addUserToHunt.ts
+++ imports/server/addUserToHunt.ts
@@ -2,6 +2,7 @@ import { Accounts } from "meteor/accounts-base";
 import { Email } from "meteor/email";
 import { Meteor } from "meteor/meteor";
 import Mustache from "mustache";
+import type { User } from "meteor/meteor";
 import Flags from "../Flags";
 import Logger from "../Logger";
 import type { HuntType } from "../lib/models/Hunts";
@@ -10,6 +11,7 @@ import Settings from "../lib/models/Settings";
 import List from "./List";
 import addUsersToDiscordRole from "./addUsersToDiscordRole";
 import { ensureHuntFolderPermission } from "./gdrive";
+import { trace } from "console";
 
 const DEFAULT_EXISTING_JOIN_SUBJECT =
   "[jolly-roger] Added to {{huntName}} on {{siteName}}";
@@ -86,15 +88,22 @@ export default async function addUserToHunt({
   invitedBy: string;
 }) {
   const huntId = hunt._id;
+  Logger.info("Adding user to hunt", { invitedBy, email, huntId });
+  let joineeUser: User | undefined | null =
+    await Accounts.findUserByEmail(email);
+  Logger.info("joineeUser", { joineeUser });
 
-  let joineeUser = Accounts.findUserByEmail(email);
   const newUser = joineeUser === undefined;
   if (!joineeUser) {
+    Logger.info("Creating new user for invitation", { email });
     const joineeUserId = await Accounts.createUserAsync({ email });
     joineeUser = await MeteorUsers.findOneAsync(joineeUserId);
   }
   if (!joineeUser?._id) {
-    throw new Meteor.Error(500, "Something has gone terribly wrong");
+    Logger.info(email);
+    Logger.info(JSON.stringify(joineeUser));
+    Logger.error("We somehow still don't have a user");
+    throw new Meteor.Error(500, "Something has gone terribly wrong", email);
   }
 
   if (joineeUser.hunts?.includes(huntId)) {
